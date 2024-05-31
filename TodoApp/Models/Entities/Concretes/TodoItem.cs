314389a579ac5984c8f8ae1c2dde00d82e505826
atamoghlanov_j <atamoghlanov_j@STHQ0124-01>
2024-05-31 19:53:16 +0400

    Add project files.

---
+++ TodoApp/Models/Entities/Concretes/TodoItem.cs
@@ -0,0 +1,15 @@
+﻿using TodoApp.Models.Entities.Commons;
+
+namespace TodoApp.Models.Entities.Concretes;
+
+public class TodoItem : BaseEntity
+{
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public bool IsCompleted { get; set; } = false;
+    
+    // Foreign Key
+    public string? UserId { get; set; }
+    // Navigation Properies
+    public User User { get; set; }
+}
---
+++ TodoApp/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TodoApp.Contexts;
+using TodoApp.Models.Entities.Concretes;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("default"));
+});
+
+builder.Services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
